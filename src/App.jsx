c7a8271c63165cce6f4296cd9a9477aa6b2c2b79
Nadim Khan <nadimkhann47@gmail.com>
2025-05-13 15:29:10 +0530

    file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import HomePage from "/src/landingPage/home/HomePage";
+import Navbar from "./LandingPage/Navbar";
+import Footer from "./LandingPage/Footer";
+import NotFound from "./LandingPage/NotFound";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
